6ea5cceedc98a3f9573a7ba4ed66c861c50a17ac
shubh-7028 <sangita333wakil@gmail.com>
2024-01-03 15:49:57 +0530

    3 files

---
+++ lambda_function.py
@@ -0,0 +1,8 @@
+import json
+This is titto
+def lambda_handler(event, context):
+    # TODO implement
+    return {
+        'statusCode': 200,
+        'body': json.dumps('Hello from Lambda!!!, git changes')
+    }
